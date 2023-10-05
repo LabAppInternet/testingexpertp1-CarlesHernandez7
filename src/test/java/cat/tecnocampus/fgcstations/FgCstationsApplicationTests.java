49b41b1f57dd2b8f826919b4ffc74e918fb6e5fb
Carles Hernandez <chernandezf@edu.tecnocampus.cat>
2023-10-05 21:13:13 +0200

    first 2

--- src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
+++ src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
@@ -1,13 +1,36 @@
 package cat.tecnocampus.fgcstations;
 
+import cat.tecnocampus.fgcstations.domain.FavoriteJourney;
+import cat.tecnocampus.fgcstations.domain.Journey;
+import cat.tecnocampus.fgcstations.domain.Station;
+import cat.tecnocampus.fgcstations.domain.User;
+import cat.tecnocampus.fgcstations.domain.exceptions.SameOriginDestinationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FgCstationsApplicationTests {
 
     @Test
-    void contextLoads() {
+    void createUser() {
+        User user = new User();
+        assertTrue(user.getFavoriteJourneyList() instanceof ArrayList<FavoriteJourney>);
+        assertTrue(user.getName()==null && user.getEmail()==null && user.getSecondName()==null && user.getUsername()==null);
+    }
+
+    @Test
+    void checkSameOriginDestinationException(){
+        Station origin = new Station();
+        Station destination = new Station();
+        origin.setNom("Mataro");
+        destination.setNom("Mataro");
+        assertThrows(SameOriginDestinationException.class, () -> new Journey(origin, destination, "123"));
     }
 
 }
