ed2523afde830d4e198f7ec1b3be651ee606a60a
CarlesHernandez7 <chernandezf@edu.tecnocampus.cat>
2023-10-07 13:17:58 +0200

    Mock

--- src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
+++ src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
@@ -1,21 +1,60 @@
 package cat.tecnocampus.fgcstations;
 
-import cat.tecnocampus.fgcstations.domain.FavoriteJourney;
-import cat.tecnocampus.fgcstations.domain.Journey;
-import cat.tecnocampus.fgcstations.domain.Station;
-import cat.tecnocampus.fgcstations.domain.User;
+import cat.tecnocampus.fgcstations.api.FGCRestController;
+import cat.tecnocampus.fgcstations.application.DTOs.FavoriteJourneyDTO;
+import cat.tecnocampus.fgcstations.application.DTOs.FriendsDTO;
+import cat.tecnocampus.fgcstations.application.FgcController;
+import cat.tecnocampus.fgcstations.domain.*;
 import cat.tecnocampus.fgcstations.domain.exceptions.SameOriginDestinationException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicReference;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = FGCRestController.class)
 class FgCstationsApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private FgcController fgcController;
+
+    @Test
+    void whenValidInput_thenReturns200() throws Exception{
+        User user = new User();
+        FavoriteJourneyDTO favJourney = new FavoriteJourneyDTO();
+        FriendsDTO friends = new FriendsDTO();
+
+        mockMvc.perform(post("/users/{userName}/favoriteJourney").contentType("aplication/json")
+                .param("FavoriteJourneyDTO","userName", "true")
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/users/friends").contentType("aplication/json")
+                .param("FriendsDTO", "true")
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+
+
+
 
     @Test
     void createUser() {
@@ -32,5 +71,4 @@ class FgCstationsApplicationTests {
         destination.setNom("Mataro");
         assertThrows(SameOriginDestinationException.class, () -> new Journey(origin, destination, "123"));
     }
-
 }
